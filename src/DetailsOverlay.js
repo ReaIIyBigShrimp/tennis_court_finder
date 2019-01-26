ce950de7ea08ffef7cafb920036a138fcba23aa6
Darryl Dolan-Rathmell <darryl.dolan@hotmail.co.uk>
2019-01-26 22:56:19 +0000

    Icons added. DetailsOverlay now displays activeCourt data.

--- src/DetailsOverlay.js
+++ src/DetailsOverlay.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
-const style = {
+const rowStyle = {
     zIndex: 500,
     position: 'absolute',
     bottom: '0px'
+}
+
+const detailsBtn = {
+    color: '#f2f2f2'
 }
 
 function DetailsOverlay({court}) {
     console.log(court);
     court != null ? console.log(court.properties) : console.log('No court data');
   return (
-    <div style={style} className="row">
+    <div style={rowStyle} className="row">
         <div className="col s12 m6">
             <div className="card blue-grey darken-1">
                 <div className="card-content white-text">
-                    <span className="card-title">{court != null ? '{court.properties.name}' : 'No court selected'}</span>
+                    <span className="card-title">{court != null ? court.properties.name : 'Select a court'}</span>
                     <p>I am a very simple card. I am good at containing small bits of information.
                     I am convenient because I require little markup to use effectively.</p>
                 </div>
                 <div className="card-action">
-                    <a href="#">This is a link</a>
-                    <a href="#">This is a link</a>
+                    <a style={detailsBtn} className="waves-effect waves-light btn"><i className="material-icons right">chevron_right</i>View More Details</a>
                 </div>
             </div>
         </div>
